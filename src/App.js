c3005aef56e75d716e8395fbb60189b096009fc6
Ganesh Patil <gp28info@gmail.com>
2024-05-16 16:59:13 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import TodoItem from './components/TodoItem';
+import TodoBtn from './components/TodoBtn';
+
+function Title(){
+  return <h1>Title for React JS by Piyush</h1>
+}
+
+function App() {
+  return (
+    <div className="App">
+        <Title />
+        <h1>|| test APP ||  </h1>        
+        <div className='todo-container'>
+          <Header title = 'Todoie App' />
+          <TodoItem item = 'Mango' />
+          <TodoItem item = 'Orange' />
+          <TodoItem item = 'Musk Bun' />
+          <TodoItem item = 'Green peas'/>
+          <TodoItem item = 'Jackfruit' />
+          <TodoBtn text = 'Add todo' />
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoBtn.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoBtn = (props) => {
+   return  <button className="todo-button"> {props.text} </button>
+}
+
+export default TodoBtn;
